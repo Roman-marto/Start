2c486ea312ad060d85d558e6b8e05aaaef9ad08a
Roman Drabushevski <133542929+Roman-marto@users.noreply.github.com>
2023-05-20 21:25:11 +0300

    Add files via upload
    Сумма квадратов первых 100 чисел

---
+++ Task133.java
@@ -0,0 +1,9 @@
+public class Task133 {
+    public static void main (String[] args) {
+        int result = 0;
+        for (int i = 0; i < 101; i++) {
+            result = result + i * i;
+        }
+        System.out.println(result);
+    }
+}
