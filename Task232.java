7b1ecad660311bce78a786f12db30397663b69d8
Roman Drabusheuski <133542929+Roman-marto@users.noreply.github.com>
2023-07-05 20:32:31 +0300

    Add files via upload

---
+++ Task232.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import static java.lang.System.*;
+
+public class Task232 {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(in);
+        out.println("Введите размер 1 массива");
+        int size1 = scan.nextInt();
+        out.println("Введите числа 1 массива");
+        int[] array1 = new int[size1];
+        for (int i = 0; i < size1; i++) {
+            array1[i] = scan.nextInt();
+        }
+        out.println("Введите размер 2 массива");
+        int size2 = scan.nextInt();
+        out.println("Введите числа 2 массива");
+        int[] array2 = new int[size2];
+        for (int i = 0; i < size2; i++) {
+            array2[i] = scan.nextInt();
+        }
+        int[] res = new int[array1.length + array2.length];
+        arraycopy(array1, 0, res, 0, array1.length);
+        arraycopy(array2, 0, res, array1.length, array2.length);
+        Arrays.sort(res);
+        out.println(Arrays.toString(array1));
+        out.println(Arrays.toString(array2));
+        out.println(Arrays.toString(res));
+    }
+}
