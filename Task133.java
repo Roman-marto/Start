3a91fe68e9be5fd37179f541dcc2993518fdb5a9
Roman Drabusheuski <133542929+Roman-marto@users.noreply.github.com>
2023-07-08 20:41:45 +0300

    Delete Task133.java

--- Task133.java
+++
@@ -1,9 +0,0 @@
-public class Task133 {
-    public static void main (String[] args) {
-        int result = 0;
-        for (int i = 0; i < 101; i++) {
-            result = result + i * i;
-        }
-        System.out.println(result);
-    }
-}
